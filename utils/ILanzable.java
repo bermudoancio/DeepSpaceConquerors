3adb69019441330a94d30b9fbfc52fff7cb5078f
José Manuel <jbermudo@iesmiravent.es>
2021-02-24 21:37:38 +0100

    Añadidos comentarios a la interfaz ILanzable

--- utils/ILanzable.java
+++ utils/ILanzable.java
@@ -1,7 +1,19 @@
 package utils;
 
 public interface ILanzable {
+	/**
+	 * Lanza el dado
+	 * @return el número generado al azar entre el mínimo y el máximo del dado
+	 */
 	public int lanzar();
+	
+	/**
+	 * @return el valor mínimo posible del dado
+	 */
 	public int getMin();
+	
+	/**
+	 * @return el valor máximo posible del dado
+	 */
 	public int getMax();
 }
