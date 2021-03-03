c187debc58032fc198410a3e2e12af9c19b895a4
José Manuel <jbermudo@iesmiravent.es>
2021-03-03 20:53:19 +0100

    Añadida documentación de las interfaces nuevas

--- spaceConquerors/ITransportable.java
+++ spaceConquerors/ITransportable.java
@@ -1,5 +1,12 @@
 package spaceConquerors;
 
 public interface ITransportable {
+	/**
+	 * Transporta un número de personas determinado al planeta destino
+	 * @param destino el planeta al que serán transportadas las personas
+	 * @param personas el número de personas a transportar
+	 * @throws InvalidValueException Si el número de personas es menor que 0 o mayor que las existentes
+	 * @throws JuegoException Si el planeta destino está conquistado por otro jugador
+	 */
 	public void transportar(Planeta destino, int personas) throws InvalidValueException, JuegoException;
 }
--- spaceConquerors/ITransportador.java
+++ spaceConquerors/ITransportador.java
@@ -1,5 +1,13 @@
 package spaceConquerors;
 
 public interface ITransportador {
+	/**
+	 * Transporta la cantidad especificada de los materiales al planeta destino
+	 * @param destino el planeta al que se va a transportar la carga
+	 * @param materiales el array de materiales. Cada elemento del array es una materia prima que se transporta
+	 * @param cantidades el array de cantidades de materias primas. Coincide en índices con el array materiales.
+	 * @throws InvalidValueException si se quiere transportar una cantidad negativa o mayor que la disponible
+	 * @throws JuegoException Si el planeta al que se va a transportar la carga está conquistado por otro jugador
+	 */
 	public void transportar(Planeta destino, TMateriales[] materiales, int[] cantidades) throws InvalidValueException, JuegoException;
 }
